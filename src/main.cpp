c141b7b6904b8339b99a0635f4b9e835545fbafc
Paul Brand <paulusbrand@gmail.com>
2024-11-15 21:03:32 +0100

    Small tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,21 @@ ModbusClientRTU MB;
 ModbusBridgeEthernet MBbridge;
 
 void setup() {
-// Init Serial2 to monitor
+  // Init Serial2 to monitor
   Serial2.begin(115200, SERIAL_8N1, GPIO_NUM_5, GPIO_NUM_17);
   while (!Serial2) {}
   Serial2.println("__ OK __");
 
-// Init Serial connected to the RTU Modbus
+  // Init Serial connected to the RTU Modbus
   RTUutils::prepareHardwareSerial(Serial);
   Serial.begin(9600);
 
-// Set RTU Modbus message timeout to 100ms
+  // Set RTU Modbus message timeout to 100ms
   MB.setTimeout(modbus_rtu_timeout);
-// Start ModbusRTU background task on core 1
+  // Start ModbusRTU background task on core 1
   MB.begin(Serial, 1);
 
-// Start ethernet
+  // Start ethernet
   int result = ETH.begin(ETH_PHY_TYPE, ETH_PHY_ADDR, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_POWER, ETH_CLK_MODE);
   ETH.setHostname(hostname);
 
@@ -46,14 +46,15 @@ void setup() {
   Serial2.print("fullDuplex: "); Serial2.println(ETH.fullDuplex());
   Serial2.print("linkSpeed: "); Serial2.println(ETH.linkSpeed());
 
-// Define and start Ethernet modbus bridge for adresses 246 and 247
+  // Define and start Ethernet modbus bridge for adresses 246 and 247
   MBbridge.attachServer(247, 247, ANY_FUNCTION_CODE, &MB);
   MBbridge.attachServer(246, 246, ANY_FUNCTION_CODE, &MB);
 
-// Start the bridge
+  // Start the bridge
   MBbridge.start(modbus_port, modbus_max_clients, modbus_client_timeout);
 
   Serial2.printf("Use the shown IP and port %d to send requests!\n", modbus_port);
 }
 
+// Do nothing
 void loop() {;;}
